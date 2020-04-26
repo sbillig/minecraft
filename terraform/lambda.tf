6227679a6078e647cf6cb213d056d18cf900da93
Dan Schoppe <dan.schoppe@gmail.com>
2020-04-26 13:26:49 -0500

    DescribeInstanceStatus

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -89,6 +89,7 @@ resource "aws_iam_policy" "logging_ec2" {
     {
       "Effect": "Allow",
       "Action": [
+        "ec2:DescribeInstanceStatus",
         "ec2:DescribeInstances",
         "ec2:StartInstances",
         "ec2:StopInstances"
